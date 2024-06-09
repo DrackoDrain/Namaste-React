d49b4d31e21644016fb8e5f6817282bf3420bc86
Ayush <ayush0999aryan@gmail.com>
2024-06-09 17:30:15 +0530

    About Parcel

---
+++ Day-1/README.md
@@ -0,0 +1,22 @@
+# Namaste React 🚀 
+
+# Parcel
+ - Dev build 
+ - Local server 
+ - HMR = Hot Module Replacement 
+ - It uses File Watching Algorithm - written in C++
+ - Cachng - Faster Builds
+ - Image Optimization
+ - Minification 
+ - Bundling 
+ - Compress
+ - Consistent Hasing 
+ - Code Splitting 
+ - Differential Bundling - Supports older browsers
+ - Diagnostic 
+ - Error Handling 
+ - HTTPs
+ - Tree Shaking - remove unused code 
+ - Different dev and production bundles 
+ 
+ 
--- Day-1/app.js
+++ Day-1/app.js
@@ -1,12 +1,8 @@
-// const heading = React.createElement("h1",
-//  { id: "heading" },
-//   "Hello World From React !"
-//   );  
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);  
+import React from "react";
+import ReactDOM from "react-dom";
 
 
-const parent = React.createElement("div",{ id : "parent"}, [React.createElement("div", {id: "child"},[React.createElement("h1", {}, "I am h1 tag"), 
+const parent = React.createElement("div",{ id : "parent"}, [React.createElement("div", {id: "child"},[React.createElement("h1", {}, "This is Namaste React 🚀"), 
   React.createElement("h2",{}, "I am h2 tag"),
 ]),
  React.createElement("div",{id: "child2"},[
