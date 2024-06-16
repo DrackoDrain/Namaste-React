3f3de0f55f6275bf43c3f2f32a9fc6aae75037ee
Ayush <ayush0999aryan@gmail.com>
2024-06-16 16:15:35 +0530

    className added

--- Day-3/app.js
+++ Day-3/app.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1",{id: "heading"}, "Hello React 🚀");
 
-const jsxHeading = <h1 id="heading"> Namaste React using JSX </h1>
+const jsxHeading = (<h1 className="heading"> Namaste React using JSX </h1>);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
