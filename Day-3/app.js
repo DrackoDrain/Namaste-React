a2395e023afee4ff7196bc2021c03d03f7e667a4
Ayush <ayush0999aryan@gmail.com>
2024-06-16 15:52:33 +0530

    JSX added

--- Day-3/app.js
+++ Day-3/app.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1",{id: "heading"}, "Hello React 🚀");
 
+const jsxHeading = <h1 id="heading"> Namaste React using JSX </h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
