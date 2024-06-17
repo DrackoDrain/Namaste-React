0fe9656e6e938a96bb4dde7fe782d8a9f3fbc0d8
Ayush <ayush0999aryan@gmail.com>
2024-06-17 22:29:18 +0530

    card data added

--- Day-4/app.js
+++ Day-4/app.js
@@ -47,12 +47,11 @@ const RestaurantCard = ({resData}) =>{
         <div className="res-card">
         <img className="res-logo" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ddn26k6s0kifkmyiesmd" }
         />
-        {/* <div><h3>Hotel Taj </h3></div> */}
         <h4> {resData.info.name} </h4>
         <h4> {resData.info.cuisines} </h4>
         <h4>{resData.info.costForTwo}</h4>
-        <h4> {resData.info.avgRating} </h4>
-        <h4> {resData.info.deliveryTime} </h4>
+        <h4> {resData.info.avgRating} stars </h4>
+        <h4> {resData.info.sla.deliveryTime} minutes </h4>
         </div>
     )
 }
